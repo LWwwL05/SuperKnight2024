06608bbc62f6f9d2f121a431973e562a2356f889
LWwwL05 <155894691+LWwwL05@users.noreply.github.com>
2024-01-08 11:19:55 +0800

    Update main.py

--- Super Knight/main.py
+++ Super Knight/main.py
@@ -1,15 +1,20 @@
 import pygame, sys
 from settings import *
+from level import Level
+
 
 class Game:
-    def __init__(self)
+    def __init__(self):
         
         #general setup
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
+        pygame.display.set_caption('Zelda')
         self.clock = pygame.time.Clock()
 
-        def run(self)
+        self.level = Level()
+
+        def run(self):
             while True:
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
@@ -17,6 +22,7 @@ class Game:
                         sys.exit()
 
                         self.screen.fill('black')
+                        self.level.run()
                         pygame.display.update()
                         self.clock.tick(FPS)
 
